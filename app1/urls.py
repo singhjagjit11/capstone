c85ed6af7010b4267ff398680c836674e8317329
james sebastian <jamessebastian916@gmail.com>
2023-03-17 09:23:26 -0400

    merged tables

--- app1/urls.py
+++ app1/urls.py
@@ -1,18 +1,15 @@
 from django.urls import include, re_path
 from app1 import views
-
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
     re_path(r'^patch$',views.patchesApi),
+        re_path(r'^display$',views.disp),
+    
     # re_path(r'^load_patch$',views.loadPatchesApi),
-    re_path(r'^display$',views.disp),
-    re_path(r'^download_csv$',views.download_csv,name='download_csv'),
     # re_path(r'^patch/([0-9]+)$',views.patchesApi),
-
     # re_path(r'^employee$',views.employeeApi),
     # re_path(r'^employee/([0-9]+)$',views.employeeApi),
-
     # re_path(r'^employee/savefile',views.SaveFile)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- app1/views.py
+++ app1/views.py
@@ -9,8 +9,6 @@ from app1.serializers import PatchesSerializer
 from django.core.files.storage import default_storage
 import requests
 
-
-
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 from .forms import UrlForm
@@ -50,7 +48,7 @@ def disp(request):
         response=requests.get('http://127.0.0.1:8000/patch').json()
         return render(request,'index4.html',{'response':response,'form':form})
 
-    #loafd from url
+    #load from url
     elif request.method=='POST':
          # Get the data from the request
          #Get the posted form
@@ -67,27 +65,23 @@ def disp(request):
             response = requests.get(url)
                     # parse the HTML content of the page
             soup = BeautifulSoup(response.content, "html.parser")
-            
+            objs=[]
             #table = soup.find('table',class_="otable-w2")# Find the table
             tables = soup.find_all("table")
-            table = tables[2]
-            # find the tbody in the table
-            tbody = table.find("tbody")
-            rows = tbody.find_all("tr")# extract the rows from the tbody
-        
-
-            objs=[]
-            for row in rows:
-                cells = row.find_all("td")
-                cells2 = row.find_all("th")
-                data2 = [cell.text for cell in cells2]
-                data = [cell.text for cell in cells]
-                data =[*data2,*data]
-                print(data)
-
-                objs.append(Patches(col0=data[0], col1=data[1],col2=data[2],col3=data[3], col4=data[4],col5=data[5],col6=data[6],
-                col7=data[7],col8=data[8],col9=data[9], col10=data[10],col11=data[11],col12=data[12],col13=data[13], col14=data[14]))
-                
+            for i in range(2,17):
+                table = tables[i]
+                tbody = table.find("tbody")
+                rows = tbody.find_all("tr")
+                for row in rows:
+                    cells = row.find_all("td")
+                    cells2 = row.find_all("th")
+                    data2 = [cell.text for cell in cells2]
+                    data = [cell.text for cell in cells]
+                    data =[*data2,*data]
+                    print(data)
+                    objs.append(Patches(col0=data[0], col1=data[1],col2=data[2],col3=data[3], col4=data[4],col5=data[5],col6=data[6],
+                    col7=data[7],col8=data[8],col9=data[9], col10=data[10],col11=data[11],col12=data[12],col13=data[13], col14=data[14]))
+                    
 
             Patches.objects.bulk_create(objs) 
             return JsonResponse("Added Successfully",safe=False)   
